ad80af82babcbe5f879080c4cc767203342e7ac2
Joyo-G <gustavo.joyo.p@gmail.com>
2023-09-16 04:26:54 -0300

    add modules for transforms

---
+++ modules/transforms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def indentidy():
+    return np.array([1.0,0.0,0.0,0.0,
+                     0.0,1.0,0.0,0.0,
+                     0.0,0.0,1.0,0.0,
+                     0.0,0.0,0.0,1.0], dtype = np.float32)
+
+def translate(dx,dy,dz):
+    return np.array([1.0,0.0,0.0,dx,
+                     0.0,1.0,0.0,dy,
+                     0.0,0.0,1.0,dz,
+                     0.0,0.0,0.0,1.0], dtype = np.float32)
