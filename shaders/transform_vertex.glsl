efdebaabf4dc1f89d1b84923f51d47de91bb290d
Joyo-G <gustavo.joyo.p@gmail.com>
2023-09-16 04:03:25 -0300

    add transform shader

--- shaders/transform_vertex.glsl
+++ shaders/transform_vertex.glsl
@@ -1,4 +1,12 @@
 #version 330
 
 in vec3 position;
-in vec4
+uniform mat4 transform = mat4(1.0);
+in vec3 color;
+
+out vec3 fragColor;
+
+void main(){
+    fragColor = color;
+    gl_Position = transform * vec4(position,1.0f);
+}
