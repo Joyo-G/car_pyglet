7d7c977581de6da13bcf09ea7b03482f0bd54128
Joyo-G <gustavo.joyo.p@gmail.com>
2023-09-16 12:23:28 -0300

    add documentation

--- tutoriales/pipeline_factory.py
+++ tutoriales/pipeline_factory.py
@@ -1,8 +1,13 @@
 import pyglet
 
 class Pipeline(pyglet.graphics.shader.ShaderProgram):
-    
+    """
+    Create and ShaderProgram with the vertex and fragment source
+    """
     def __init__(self,vertex_souce, fragment_source):
+        """
+        Initialize an ShaderProgram with the vertex and fragment source as shader
+        """
         self.__vertex = pyglet.graphics.shader.Shader(vertex_souce,"vertex")
         self.__fragment = pyglet.graphics.shader.Shader(fragment_source,"fragment")
         super().__init__(self.__vertex,self.__fragment)
