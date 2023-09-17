cda1036e25c5acafcda2576f4019d958cb34359d
Joyo-G <gustavo.joyo.p@gmail.com>
2023-09-16 21:48:11 -0300

    add rotations

--- modules/transforms.py
+++ modules/transforms.py
@@ -17,3 +17,36 @@ def uniformScale(s_u):
                      [0.0,s_u,0.0,0.0],
                      [0.0,0.0,s_u,0.0],
                      [0.0,0.0,0.0,1.0]], dtype = np.float32)
+
+def uniformScale(s_x,s_y,s_z):
+    return np.array([[s_x,0.0,0.0,0.0],
+                     [0.0,s_y,0.0,0.0],
+                     [0.0,0.0,s_z,0.0],
+                     [0.0,0.0,0.0,1.0]], dtype = np.float32)
+
+def rotateZ(theta):
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    return np.array([
+        [cos_theta,-sin_theta,0,0],
+        [sin_theta,cos_theta,0,0],
+        [0,0,1,0],
+        [0,0,0,1]], dtype = np.float32)
+
+def rotateX(theta):
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    return np.array([
+        [1,0,0,0],
+        [0,cos_theta,-sin_theta,0],
+        [0,sin_theta,cos_theta,0],
+        [0,0,0,1]], dtype = np.float32)
+
+def rotateY(theta):
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    return np.array([
+        [cos_theta,0,sin_theta,0],
+        [0,1,0,0],
+        [-sin_theta,0,cos_theta,0],
+        [0,0,0,1]], dtype = np.float32)
