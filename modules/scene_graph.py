a295ef84237671607a68233374df7fab1a9e147a
Joyo-G <gustavo.joyo.p@gmail.com>
2023-10-22 17:30:51 -0300

    add scene graph class and defined add_graph, add_node and __getitem__ methods

---
+++ modules/scene_graph.py
@@ -0,0 +1,40 @@
+"""
+This module implements Scene Graphs using NetworkX
+"""
+
+from typing import Mapping
+import networkx as nt
+import numpy as np
+import transforms as tr
+__autor__ = "Gustavo Joyo"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+class SceneGraph(nt.DiGraph):
+    """
+     Scene Graph
+    """
+    def __init__(self,incoming_graph_data = None,**args):
+        super().__init__(incoming_graph_data,**args)
+        super().add_node("root")
+    """
+    Add a graph to root node
+    """
+    def add_graph(self,graph,root="root"):
+        self.add_node(graph)
+        self.add_edge(root,graph)
+
+    """
+    Add node and its attributes
+    """
+    def add_node(self,node,root="root",mesh=None,transform=tr.indentidy()):
+        super().add_node(node,mesh=mesh,transform=transform)
+        super().add_edge(root,node)
+
+    """
+    Replace use of graph.nodes[node] for get attributes of node using only graph[node]
+    """
+    def __getitem__(self, node):
+        if node not in self.nodes:
+            raise KeyError(f"{node} not in graph")
+        return self.nodes[node]
