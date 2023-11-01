e1e66e9b624db0a8b3f5db5f853ff585b996d931
Joyo-G <gustavo.joyo.p@gmail.com>
2023-11-01 10:12:18 -0300

    testing now scene_graph

--- modules/scene_graph.py
+++ modules/scene_graph.py
@@ -1,5 +1,5 @@
 """
-This module implements Scene Graphs using NetworkX
+This module implements an Scene Graph
 """
 
 import networkx as nt
@@ -9,4 +9,63 @@ from shapes.model import Model
 __autor__ = "Gustavo Joyo"
 __license__ = "MIT"
 __version__ = "1.0.0"
+
+
+class Leaf():
+
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class SceneNode():
+
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+class SceneGraph():
+    """
+    Scene graph
+    """
+    def __init__(self):
+        self.childs = []
+        self.transform = tr.identidy()
+
+    """
+    Draw the scene graph
+    """
+    def draw(self):
+        for node in self.childs:
+            node.draw()
+
+    def add_node(self,node):
+        pass
+    
+    def getTransform(self):
+        return self.transform
+
+    def update(self):
+        for node in self.childs:
+            self.transform@=node.getTransform()
+
+
+
+###Test
+
+arbol = SceneGraph()
+
+triangulo = Triangle()
+triangulo.init_gpu_data()
+
+cuadrado = Squad()
+cuadrado.init_gpu_data()
+
+arbol.add_node("hojas",transformation = ...)
+arbol.add_node("tronco",cuadrado)
+
 
